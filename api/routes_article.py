4efe374d8b18bb6e32430ca8184487246b77bdad
arslan.akautdinov <arslan.akautdinov@rt.ru>
2020-10-27 18:47:15 +0300

    fix

--- api/routes_article.py
+++ api/routes_article.py
@@ -1,5 +1,3 @@
-from functools import wraps
-
 import jwt
 import sqlalchemy.exc
 import json
@@ -13,14 +11,12 @@ from db.client import db_session
 from db.models.article import Article
 
 
-base_headers = {"Content-Type": "application/json"}
-
-
 def check_for_token(headers):
-    if "token" not in headers:
+    if "Authorization" not in headers:
         return jsonify({"error": "Missing token"}), 403
     try:
-        decoded = jwt.decode(headers["token"], app.config["SECRET_KEY"])
+        token = headers["Authorization"]
+        decoded = jwt.decode(token, app.config["SECRET_KEY"])
         return {"user_id": decoded["user_id"]}
     except Exception as ex:
         return jsonify({"error": "Invalid token"}), 403
@@ -30,15 +26,30 @@ def check_for_token(headers):
 @swag_from(os.path.join(SWAGGER_DIR, "get_article.yml"))
 def get_article():
     try:
+        selected_articles: list = Article.query.filter_by().all()
+        if len(selected_articles) != 0:
+            return make_response(json.dumps([article.get_dict() for article in selected_articles]), 200, {"Content-Type": "application/json"})
+        else:
+            return make_response([], 200, {"Content-Type": "application/json"})
+    except sqlalchemy.exc.IntegrityError as e:
+        return make_response(jsonify({"errors": e.args}), 422)
+    except Exception as e:
+        return make_response(jsonify({"errors": e}), 500)
+
+
+@app.route("/api/v1/article/author", methods=["GET"])
+@swag_from(os.path.join(SWAGGER_DIR, "get_article_user.yml"))
+def get_article_author():
+    try:
         if "user_id" in check_for_token(request.headers):
-            user_id = check_for_token(request.headers)
+            user_id = check_for_token(request.headers)["user_id"]
         else:
             return check_for_token(request.headers)
-        selected_articles: list = Article.query.filter_by(author_id=user_id).all()
+        selected_articles: list = Article.query.filter_by(Article.author_id == user_id).all()
         if len(selected_articles) != 0:
-            return make_response(json.dumps([article.get_dict() for article in selected_articles]), 200, base_headers)
+            return make_response(json.dumps([article.get_dict() for article in selected_articles]), 200, {"Content-Type": "application/json"})
         else:
-            return make_response([], 200, base_headers)
+            return make_response([], 200, {"Content-Type": "application/json"})
     except sqlalchemy.exc.IntegrityError as e:
         return make_response(jsonify({"errors": e.args}), 422)
     except Exception as e:
@@ -49,7 +60,10 @@ def get_article():
 @swag_from(os.path.join(SWAGGER_DIR, "add_article.yml"))
 def add_article():
     try:
+        if "user_id" in check_for_token(request.headers): user_id = check_for_token(request.headers)["user_id"]
+        else: return check_for_token(request.headers)
         created_article = Article(**request.get_json())
+        created_article.author_id = user_id
         db_session.add(created_article)
         db_session.commit()
         db_session.refresh(created_article)
@@ -66,9 +80,28 @@ def get_article_by_id(article_id: int):
     try:
         selected_article: Article = Article.query.filter_by(id=article_id).first()
         if selected_article is not None:
-            return make_response(json.dumps(selected_article.get_dict()), 200, base_headers)
+            return make_response(json.dumps(selected_article.get_dict()), 200, {"Content-Type": "application/json"})
         else:
-            return make_response(jsonify({"errors": "not found."}), 404, base_headers)
+            return make_response(jsonify({"errors": "not found."}), 404, {"Content-Type": "application/json"})
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        return make_response(jsonify({"errors": e.args}), 422)
+    except Exception as e:
+        return make_response(jsonify({"errors": e}), 500)
+
+
+@app.route("/api/v1/article/author/<article_id>", methods=["GET"])
+@swag_from(os.path.join(SWAGGER_DIR, "get_article_user_by_id.yml"))
+def get_article_author_by_id(article_id: int):
+    try:
+        if "user_id" in check_for_token(request.headers):
+            user_id = check_for_token(request.headers)["user_id"]
+        else:
+            return check_for_token(request.headers)
+        selected_article: Article = Article.query.filter(Article.id == article_id, Article.author_id == user_id).first()
+        if selected_article is not None:
+            return make_response(json.dumps(selected_article.get_dict()), 200, {"Content-Type": "application/json"})
+        else:
+            return make_response(jsonify({"errors": "not found."}), 404, {"Content-Type": "application/json"})
     except sqlalchemy.exc.SQLAlchemyError as e:
         return make_response(jsonify({"errors": e.args}), 422)
     except Exception as e:
@@ -79,8 +112,16 @@ def get_article_by_id(article_id: int):
 @swag_from(os.path.join(SWAGGER_DIR, "del_article.yml"))
 def del_article_by_id(article_id: int):
     try:
-        Article.query.filter_by(id=article_id).delete()
-        db_session.commit()
+        if "user_id" in check_for_token(request.headers):
+            user_id = check_for_token(request.headers)["user_id"]
+        else:
+            return check_for_token(request.headers)
+        result = Article.query.filter(Article.id == article_id, Article.author_id == user_id).first()
+        if result is not None:
+            Article.query.filter(Article.id == article_id, Article.author_id == user_id).delete()
+            db_session.commit()
+        else:
+            return make_response(jsonify({"errors": "not found"}), 404)
         return make_response({"message": "ok"}, 200)
     except sqlalchemy.exc.SQLAlchemyError as e:
         return make_response(jsonify({"errors": e.args}), 422)
--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-
 from flasgger import Swagger
 from flask import Flask
 
 swagger_template = {
     'securityDefinitions': {
-        "BearerAuth": {
-            "name": "authorization",
+        "Token": {
+            "name": "Authorization",
             "type": "apiKey",
             "scheme": "bearer",
             "bearerFormat": "JWT",
@@ -15,11 +14,13 @@ swagger_template = {
     }
 }
 app = Flask(__name__)
-swagger = Swagger(app, template=swagger_template)
-
+app.config['SWAGGER'] = {
+    'title': 'Load mock service API 0.1',
+    'description': "Данный сервис предназначен для проведения пробных нагрузочных тестов.",
+    'uiversion': 3
+}
 app.config["SECRET_KEY"] = "thisismysecretkey"
 app.config["DATABASE_URL"] = 'postgresql://admin:admin@127.0.0.1/lock_service'
-
-
+swagger = Swagger(app, template=swagger_template)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SWAGGER_DIR = os.path.join(BASE_DIR, "swagger")
