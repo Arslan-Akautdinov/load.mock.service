11a9a8589be8e5e0e13a821d10c12516ea441c00
arslan.akautdinov <arslan.akautdinov@rt.ru>
2020-10-29 00:49:07 +0300

    fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from app import app
 
 import api.routes_article
 import api.routes_user
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(port=os.environ.get("APP_PORT"), debug=True)
